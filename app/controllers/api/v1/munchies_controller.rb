fff39eb59324b9e033d75242dac6fc9ee980de9e
billy urrutia <billyurrutia@gmail.com>
2019-07-29 10:59:24 -0600

    updated arguements to include food

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     time_to_travel = RoadTripService.new(params[:start], params[:end]).get_time
     time_for_yelp_service = Time.now.to_i + time_to_travel[:duration][:value]
-    render json: YelpService.new(time_for_yelp_service, time_to_travel).get_results
+    render json: YelpService.new(time_for_yelp_service, time_to_travel, params[:food]).get_results
   end 
 end
