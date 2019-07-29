0c767d272ff42e3301279076b92d7cf96fd85588
billy urrutia <billyurrutia@gmail.com>
2019-07-29 09:46:42 -0600

    route created for munchies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       get 'forecast', to: 'forecast#show' 
+      get 'munchies', to: 'munchies#index'
     end
   end
 end
