3427fa5e11f325fb7ef2df42e0fc01a7c6ddea8f
billy urrutia <billyurrutia@gmail.com>
2019-07-29 12:06:09 -0600

    Still looks ugly will refactor more

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    render json: YelpSerializer.new(Yelp.new(yelp_service))
+    render json: YelpSerializer.new(yelp_attributes)
   end 
 
   private 
@@ -11,6 +11,12 @@ class Api::V1::MunchiesController < ApplicationController
 
   def yelp_service 
     time_for_yelp_service = Time.now.to_i + get_time_to_travel[:duration][:value]
-    yelp_service ||= YelpService.new(time_for_yelp_service, get_time_to_travel, params[:food]).get_3_restaurants
+    yelp_service ||= YelpService.new(time_for_yelp_service, get_time_to_travel, params[:food]).get_restaurants_by_count(3)
+  end
+
+  def yelp_attributes
+    yelp_service[:businesses].map do |business|
+      YelpRestaurant.new(business)
+    end
   end
 end
