d471637cfe8f1cdffd2b68cbb67fb8b34675180b
billy urrutia <billyurrutia@gmail.com>
2019-07-29 10:59:09 -0600

    Need to add bearer token

---
+++ app/services/yelp_service.rb
@@ -0,0 +1,24 @@
+class YelpService
+  def initialize(time, hash_of_info, food)
+    @open_at = time
+    @location = hash_of_info[:end_address]
+    @food = food
+  end
+
+  def get_results
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private 
+
+  def conn 
+    Faraday.new('https://api.yelp.com/v3/businesses/search') do |f|
+      f.params['location'] = @location
+      f.params['open_at'] = @open_at
+      f.params['term'] = @food
+      f.adapter Faraday.default_adapter
+    end
+  end  
+
+end
