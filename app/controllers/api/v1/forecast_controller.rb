ea5c9d04d7838547e3a9d673449cdd697a93e578
billy urrutia <billyurrutia@gmail.com>
2019-07-29 21:56:28 -0600

    added fixture for model testing

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def show
     coordinates = GeocodeService.new(params[:location]).lat_lng
     json = ForecastService.new(coordinates).get_forecast_results 
-    render json: ForecastSerializer.new(Forecast.new(json))
+    render json: ForecastSerializer.new(Forecast.new(json, params[:location]))
   end
 
 end
