5c992a6dd4615f759e6fe810afe7f86b8e2ca128
billy urrutia <billyurrutia@gmail.com>
2019-07-30 23:18:57 -0600

    created background_photo controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::BackgroundsController < ApplicationController
-  def show
-    binding.pry
-    photo = BackgroudPhoto.new(BackgroundService.new(params[:location]).get_results)
-    render json: BackgroundPhotoSerializer.new(BackgroudPhoto.new(photo))
-  end 
-end
