1c145faaacab1c63f8a9dd4423d29e5422fe34ac
billy urrutia <billyurrutia@gmail.com>
2019-07-31 09:42:45 -0600

    refactored munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,22 +1,5 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    render json: YelpSerializer.new(yelp_attributes)
+    render json: YelpSerializer.new(MunchiesFacade.new(params).yelp_restaurants)
   end 
-
-  private 
-
-  def get_time_to_travel
-    @get_time_to_travel ||= RoadTripService.new(params[:start], params[:end]).get_time
-  end
-
-  def yelp_service 
-    time_for_yelp_service = Time.now.to_i + get_time_to_travel[:duration][:value]
-    yelp_service ||= YelpService.new(time_for_yelp_service, get_time_to_travel, params[:food]).get_restaurants_by_count(3)
-  end
-
-  def yelp_attributes
-    yelp_service[:businesses].map do |business|
-      YelpRestaurant.new(business)
-    end
-  end
 end
